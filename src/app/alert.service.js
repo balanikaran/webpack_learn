3bf2484104d0ac254a6dc1555060fdb020a9292e
Karan Balani <karankbalani@gmail.com>
2021-04-21 00:56:18 +0530

    code split - no webpack

---
+++ src/app/alert.service.js
@@ -0,0 +1,25 @@
+class AlertService {
+  constructor() {
+    this.errorBox = document.getElementById("error");
+  }
+
+  handleAdditionError(inputs, numbers) {
+    console.log("i was called");
+    const fullMessage = inputs.reduce((message, str, index) => {
+      if (inputsAreValid(numbers[index])) {
+        return message + "";
+      } else {
+        return message + `${str} is not a number`;
+      }
+    }, "Please enter two valid numbers!");
+
+    this.errorBox.classList.remove("invisible");
+    this.errorBox.innerText = fullMessage;
+  }
+
+  hideErrors() {
+    console.log(" i ran");
+    console.log(this.errorBox);
+    this.errorBox.classList.add("invisible");
+  }
+}
---
+++ src/app/component.service.js
@@ -0,0 +1,20 @@
+class ComponentService {
+  constructor() {
+    this.numberOneInput = document.getElementById("numberOne");
+    this.numberTwoInput = document.getElementById("numberTwo");
+    this.addValuesBtn = document.getElementById("addValues");
+    this.resultDiv = document.getElementById("result");
+  }
+
+  getInputs() {
+    return [this.numberOneInput.value, this.numberTwoInput.value];
+  }
+
+  setResult(str) {
+    this.resultDiv.innerText = str;
+  }
+
+  onClick(callBackFn) {
+    this.addValuesBtn.addEventListener("click", callBackFn);
+  }
+}
